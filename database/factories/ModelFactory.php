e5fcb3a72ae453a657c18f013a1735ec90c13b37
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2015-11-24 17:29:08 +0700

    model factory
    factory for company

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,22 @@ $factory->define(Akuntansi\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(Akuntansi\Models\Company::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->company,
+        'address'=> $faker->address,
+        'telephone'=> $faker->phoneNumber,
+        'email'=> $faker->companyEmail,
+        // karena tidak ada pilihan untuk Taxpayer Registration Number
+        'npwp'=> $faker->creditCardNumber,
+        // n Numeric representation of a month, without leading zeros
+        // menggunakan date() bukan $faker->year|month karena
+        // inggin mendapatkan bulan dan tahun sekarang bukan bulan dan tahun acak.
+        'month_period'=> date("n"),
+        'year_period'=>date("Y"),
+        'information'=>"company",
+        // default 0. karena, field ini digunakan untuk menggetahui siapa yang merubah data.
+        'user_id'=>0,
+    ];
+});
